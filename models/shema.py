caeeb0135a10eba4f9167ccf62218ee7911618ca
Gaziz Malikov <Gazizmalikov0@gmail.com>
2025-03-28 11:02:51 +0500

    Update files

--- models/shema.py
+++ models/shema.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 from fastapi import Path
 
 class RegisterUser(BaseModel):
     first_name: str = Path(max_length=15)
     last_name: str | None = Path(max_length=20)
     email: EmailStr
-    password: str
+    password: str = Path(max_length=20)
     again_password: str
 
 class LoginUser(BaseModel):
     email: EmailStr
     password: str = Path(max_length=20)
+    
+class UpdateUser(BaseModel):
+    first_name: str | None = Path(max_length=15)
+    last_name: str | None = Path(max_length=20)
+    password: str | None = Path(max_length=20)
--- routers/update.py
+++ routers/update.py
@@ -1,26 +1,26 @@
 from fastapi import APIRouter, HTTPException
-
+from typing import Optional
 from Database.fake_database import users
-from models.shema import RegisterUser
+from models.shema import UpdateUser
 
 update_user_router = APIRouter(prefix="/update", tags=["Редактирование"])
 
 
 @update_user_router.put("/{user_id}")
-async def update_user(*, user_id: str, user: RegisterUser):
-    if user_id != users[user_id]:
+async def update_user(*, user_id: int, user: Optional[UpdateUser] = None):
+    if user_id not in users:
         raise HTTPException(status_code=404, detail="Not found")
     
-    if user.first_name != None:
-        users["first_name"] = user.first_name
+    if user is None:
+        return {"Data": "No data provided for update"}
     
-    if user.last_name != None:
-        users["last_name"] = user.last_name
+    if user.first_name is not None:
+        users[user_id]["first_name"] = user.first_name
     
-    if user.email != None:
-        users["email"] = user.email
+    if user.last_name is not None:
+        users[user_id]["last_name"] = user.last_name
         
-    if user.password != None:
-        users["password"] = user.password
+    if user.password is not None:
+        users[user_id]["password"] = user.password
     
     return {"Data": "success"}
